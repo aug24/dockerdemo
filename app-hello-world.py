4369ac65b27ca609f86052f99e9d64a62830306b
Justin Rowles <justin.rowles@bgch.co.uk>
2015-07-14 11:34:49 +0100

    Revert to basic

--- app-hello-world.py
+++ app-hello-world.py
@@ -2,6 +2,6 @@ import bottle
  
 @bottle.route('/')
 def home_page():
-    return "Hello everyone - listening on port 5000 with my even newer feature\n"
+    return "Hello world - listening on port 5000\n"
  
 bottle.run(host='0.0.0.0',port=5000)
