543d2b7406035d009176016065110a1437cdbe06
Justin Rowles <justin.rowles@bgch.co.uk>
2015-07-14 11:32:19 +0100

    Added newer feature

--- app-hello-world.py
+++ app-hello-world.py
@@ -2,6 +2,6 @@ import bottle
  
 @bottle.route('/')
 def home_page():
-    return "Hello everyone - listening on port 5000 with my new feature\n"
+    return "Hello everyone - listening on port 5000 with my even newer feature\n"
  
 bottle.run(host='0.0.0.0',port=5000)
